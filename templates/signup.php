f530442e9fe7c1ee4626d296ca3d37344ed194da
constantinlungu <constantin.lungu1@yahoo.com>
2018-06-01 20:31:33 +0300

    Modificare algoritm abonare la newsletter
    Am folosit PHPMailer in loc de functia mail(), fiind mai eficient si mai
    rapid.

--- templates/signup.php
+++ templates/signup.php
@@ -1,4 +1,10 @@
 <?php
+    
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    //Load Composer's autoloader
+    require '../vendor/autoload.php';
 
     $db = mysqli_connect ("golar3.go.ro", "tw", "59885236", "CriC");
     
@@ -20,11 +26,34 @@
 
     $sql = "INSERT INTO signup(mail, oras) VALUES ('$email', '$oras')";
 
-    $subj = "Abonare la newsletter";
-    $msg = "V-ati abonat cu succes la newsletter-ul site-ului !\nOrasul dumneavoastra este: $oras";
-    mail($email,$subj,$msg,'From: crisis.containment@gmail.com');
-
     mysqli_query ($db, $sql);
+
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'crisis.containment@gmail.com';                 // SMTP username
+        $mail->Password = 'crisis123';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom('crisis.containment@gmail.com', 'Crisis Containment Service');
+        $mail->addAddress($email);     // Add a recipient
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = "Abonare la newsletter";
+        $mail->Body    = "V-ati abonat cu succes la newsletter-ul site-ului !<br>Orasul dumneavoastra este: <b>$oras</b>";
+        $mail->AltBody = "V-ati abonat cu succes la newsletter-ul site-ului !\nOrasul dumneavoastra este: $oras";
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }
 
     header ('Location: ../index.php');
 
